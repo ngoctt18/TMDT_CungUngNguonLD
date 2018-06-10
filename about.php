23e270e9f5a2e0ae8cd3620e1cc923e1755e8eb5
loducnt2 <loducnt2@gmail.com>
2018-06-10 09:08:14 +0700

    fix contact va about

--- about.php
+++ about.php
@@ -22,15 +22,27 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe nostrum ullam eveniet pariatur voluptates odit, fuga atque ea nobis sit soluta odio, adipisci quas excepturi maxime quae totam ducimus consectetur?</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius praesentium recusandae illo eaque architecto error, repellendus iusto reprehenderit, doloribus, minus sunt. Numquam at quae voluptatum in officia voluptas voluptatibus, minus!</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut consequuntur magnam, excepturi aliquid ex itaque esse est vero natus quae optio aperiam soluta voluptatibus corporis atque iste neque sit tempora!</p>
+          <p>Chúng tôi cấp dịch vụ cung ứng nguồn lao động quốc tế (Nhật Bản, Australia, Hàn Quốc, Đài Loan, Singapore,…) cho người lao động, thực tập sinh, lao động kỹ thuật và chuyên gia có nhu cầu. Tại nước ta, hàng năm có hàng trăm nghìn lao động tham gia vào các chương trình xuất khẩu lao động, tuy nhiên không phải ai cũng được cung cấp đầy đủ thông tin, cũng như được cam kết đảm bảo mức lương, thu nhập, các quyền và lợi ích hợp pháp khi làm việc tại thị trường xuất khẩu lao động mà họ tham gia.</p>
+
+          <p>Chúng tôi liên tục tổ chức tuyển chọn, đào tạo ngoại ngữ, chuyên môn, tay nghề và kiến thức văn hoá cần thiết cho mọi đối tượng từ lao động phổ thông đến lao động kỹ thuật và chuyên gia đáp ứng yêu cầu về nhân lực của các doanh nghiệp, tập đoàn lớn tại thị trường lao động trong nước cũng như là quốc tế; đảm bảo luôn cung cấp các thông tin kịp thời, chuẩn xác nhất từ tất cả các thị trường việc làm để người lao động nắm rõ hơn các cơ hội việc làm cũng như là xu thế việc làm mwois nhất, tránh các rủi ro có thể xảy đến với người lao động</p>
+
+          <p>Lấy sự phục vụ tận tâm làm nền tảng hoạt động, chú trọng nâng cao trình độ chuyên môn và văn hóa cho người lao động, đồng thời cam kết đảm bảo mức lương, các quyền và lợi ích hợp pháp cho người lao động trong nước và ngoài nước.</p>
+        </div>
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <p><b>Những lợi ích khi lựa chọn chúng tôi</b></p>
+          <ul ul style=”list-style-type: circle;”>
+            <li>Là công ty uy tín, là thương hiệu tin cậy cho người lao động.</li>
+            <li>Người lao động sẽ được đào tạo trong một môi trường thân thiện với trang bị vật chất và cơ sở học liệu đầy đủ</li>
+            <li>Luôn cập nhật các công việc mới hấp dẫn cho người lao động</li>
+            <li>Chúng tôi luôn mở rộng quan hệ hợp tác với các đối tác, các tổ chức trong và ngoài nước.</li>
+            <li>Luôn có những chính sách hỗ trợ cho người lao động khi làm việc tại nước ngoài để đảm bảo mức lương, thu nhập, các quyền và lợi ích hợp pháp; luôn đồng hành cùng với người lao động trong mọi hoàn cảnh.</li>
+            <li>Cam kết tiếp tục giới thiệu việc làm cho người lao động sau khi về nước.</li>
+          </ul>
         </div>
       </div>
     </div>
 
     <hr>
-
 
 
 <?php
